95ea501c74549e366d7d67c1573f22f7d09874d6
allaev <34710484+allaev@users.noreply.github.com>
2018-03-29 20:54:09 +0500

    add comments routes and views

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.get("/campgrounds", function(req, res){
             console.log(err);
         }
         else {
-            res.render("index", {campgrounds:allCampgrounds});
+            res.render("campgrounds/index", {campgrounds:allCampgrounds});
         }
     });
 });
@@ -48,7 +48,7 @@ app.post("/campgrounds", function(req,res){
 
 //NEW - show form to create a new campground
 app.get("/campgrounds/new", function(req,res){
-    res.render("new");
+    res.render("campgrounds/new");
 });
 
 //SHOW - shows more info about one campground
@@ -61,9 +61,25 @@ app.get("/campgrounds/:id", function(req,res){
         else {
             console.log(foundCampground);
             //render show template with that campground
-            res.render("show", {campground: foundCampground});
+            res.render("campgrounds/show", {campground: foundCampground});
         }
     });
+});
+
+// =================
+// COMMENTS ROUTES
+// =================
+
+app.get("/campgrounds/:id/comments/new", function(req, res){
+    //find campground by id
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err)
+        }
+        else{
+            res.render("comments/new", {campground: campground}); 
+        }
+    })
 });
 
 app.listen(process.env.PORT, process.env.IP, function(){
