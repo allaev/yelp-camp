8bebf469c5ebd428e7f399019fcecf51bc61accd
allaev <34710484+allaev@users.noreply.github.com>
2018-04-01 16:52:30 +0500

    update error handling
    +show an error message, if campground or comment not found

--- middleware/index.js
+++ middleware/index.js
@@ -6,7 +6,7 @@ var middlewareObj = {};
 middlewareObj.checkCampgroundOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Campground.findById(req.params.id, function(err, foundCampground){
-            if(err){
+            if(err || !foundCampground){
                 req.flash("error", "Campground not found");
                 res.redirect("back");
             }
@@ -31,8 +31,8 @@ middlewareObj.checkCampgroundOwnership = function(req, res, next){
 middlewareObj.checkCommentOwnership = function(req, res, next){
     if(req.isAuthenticated()){
         Comment.findById(req.params.comment_id, function(err, foundComment){
-            if(err){
-                req.flash("error", "You need to be logged in to do that");
+            if(err || !foundComment){
+                req.flash("error", "Comment not found");
                 res.redirect("back");
             }
             else {
@@ -48,6 +48,7 @@ middlewareObj.checkCommentOwnership = function(req, res, next){
         });
     }
     else {
+        req.flash("error", "You need to be logged in to do that");
         res.redirect("back");
     }
 }
